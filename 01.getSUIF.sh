987d90083d732b7fd9e3803c89c1eaeef5ca7d87
Myhael76 <48013126+Myhael76@users.noreply.github.com>
2022-06-27 19:58:34 +0200

    some more defense + the required wsh file

--- 01.getSUIF.sh
+++ 01.getSUIF.sh
@@ -20,3 +20,14 @@ fi
 . "${SUIF_CACHE_HOME}/01.scripts/commonFunctions.sh"
 
 huntForSuifFile "01.scripts/installation" "setupFunctions.sh" || exit 2
+
+if [ ! -f "${SUIF_CACHE_HOME}/01.scripts/installation/setupFunctions.sh" ]; then
+  logE "Error downloading file ${SUIF_CACHE_HOME}/01.scripts/installation/setupFunctions.sh"
+  ls -l "${SUIF_CACHE_HOME}/01.scripts/"
+  ls -l "${SUIF_CACHE_HOME}/01.scripts/installation"
+  exit 3
+fi
+
+chmod u+x "${SUIF_CACHE_HOME}/01.scripts/installation/setupFunctions.sh"
+
+huntForSuifFile "01.scripts\pwsh" "generateInventoryFileFromInstallScript.ps1" || exit 4
